10e83c74257b550016c71595d18636f2a5caf718
Luke Hewitt <lbh@Lukes-MacBook-Pro.local>
2018-08-06 13:57:50 -0700

    Update API, clean up

--- example_czx.py
+++ example_czx.py
@@ -5,13 +5,52 @@ import torch
 from torch import nn, optim
 from torch.distributions.normal import Normal
 
-from vhe import VHE, DataLoader, Factors, Result
+from vhe import VHE, DataLoader, Transform
+
+# This toy example demonstrates how to train a Variational Homoencoder (VHE) for data organised into classes
+#
+# We generate a synthetic dataset where:
+#   Each example x is a pair floats: x = (a,b)
+#   Each class contains shifted versions of the same (a,b): [(a+d1, b+d1), (a+d2, b+d2), ...]
+#       That is to say, the difference (b-a) is constant for all elements in a class
+#
+# For the VHE, we use a simple hierarchical linear model:
+#  _______
+# |       |  c_i ~ Normal(0, 1) for each class i
+# |  (c)  |
+# | __|__ |
+# ||  |  ||
+# || (z) ||  z_ij ~ Normal(0, 1) for each element j
+# ||  |  ||
+# || (x) ||  x_ij ~ Normal( mu = A*[c_i, z_ij] + b,
+# ||_____||                 sigma = softplus(C*[c_i, z_ij] + d) )
+# |_______|
+#
+# The encoder network for c is a linear function of the support set D (averaged over elements)
+# The encoder network for z is a linear function of x
+#
+# ---------------------------
+#    Writing distributions
+# ---------------------------
+# Every distribution (encoders, decoders, prior) should be an nn.Module which implements:
+#     forward(self, [inputs,] *args, <variable>=None)
+# Arguments:
+#   First argument is 'inputs' for an encoder (batch * |D| * ...)
+#   <variable> is the random variable to be scored/sampled by the distribution
+#   args are latent variables which <variable> depends on
+# Returns:
+#   A tuple: (value, log_prob)
+#   If <variable> is None: sample value
+#   Otherwise: value = <variable>
 
 
-# Model
-x_dim = 5
-c_dim = 10
-z_dim = 10
+
+
+## ----- Model definition -----
+Dc = 2 # Number of elements in the support set D (i.e. number of elements input to encoder q_c)
+x_dim = 2
+c_dim = 3
+z_dim = 1
 h_dim = 10
 
 class Px(nn.Module):
@@ -24,7 +63,7 @@ class Px(nn.Module):
         cz = torch.cat([c,z], dim=1)
         dist = Normal(self.mu(cz), self.sigma(cz))
         if x is None: x = dist.rsample()
-        return Result(x, dist.log_prob(x).sum(dim=1))
+        return x, dist.log_prob(x).sum(dim=1) # Return value, score
 
 class Qc(nn.Module):
     def __init__(self):
@@ -41,7 +80,7 @@ class Qc(nn.Module):
         sigma_c = self.sigma_c(mean_embedding)
         dist = Normal(mu_c, sigma_c)
         if c is None: c = dist.rsample()
-        return Result(c, dist.log_prob(c).sum(dim=1))
+        return c, dist.log_prob(c).sum(dim=1) # Return value, score
 
 class Qz(nn.Module):
     def __init__(self):
@@ -54,44 +93,50 @@ class Qz(nn.Module):
         sigma_z = self.sigma_z(inputs[:, 0])
         dist = Normal(mu_z, sigma_z)
         if z is None: z = dist.rsample()
-        return Result(z, dist.log_prob(z).sum(dim=1))
+        return z, dist.log_prob(z).sum(dim=1) # Return value, score
 
-encoder = Factors(c=Qc(), z=Qz())
-decoder = Px()
-vhe = VHE(encoder, decoder)
+vhe = VHE(encoder=[Qc(), Qz()], decoder=Px()) #Use default prior c,z ~ N(0, 1)
 
-# Generate dataset
+
+## ----- Generate dataset -----
 n = 0
-classes = []
-for i in range(1000):
-    mu = torch.randn(1, x_dim)
-    sigma = 0.1
-    class_size = random.randint(10,20)
-    classes.append(mu + sigma*torch.randn(class_size, x_dim))
+def generateClass():
+    class_size = random.randint(10,20) # Randomly sample between 10 and 20 examples of each class
+    mu = torch.randn(1, x_dim) + 3
+    offset = torch.randn(class_size, 1)
+    return mu + offset
+classes = [generateClass() for i in range(1000)]
 data = torch.cat(classes)
 class_labels = [i for i in range(len(classes)) for j in range(len(classes[i]))] 
 
 
-# Training
+## ----- Data loader creates minibatches of data -----
 batch_size = 100
-n_inputs = 1
-data_loader = DataLoader(data=data,
-        labels={"c":class_labels, "z":range(len(data))},
-        k_shot={"c":5, "z":1},
-        batch_size=batch_size)
+data_loader = DataLoader(
+        data = data,
+        labels = {"c":class_labels, "z":range(len(data))},
+        k_shot = {"c":Dc, "z":1},
+        batch_size = batch_size)
 
-
-# Training
+## ----- Training -----
+nEpochs = 50
 optimiser = optim.Adam(vhe.parameters(), lr=1e-3)
-for epoch in range(1,11):
+for epoch in range(nEpochs):
     for batch in data_loader:
         optimiser.zero_grad()
         score, kl = vhe.score(inputs=batch.inputs, sizes=batch.sizes, x=batch.target, return_kl=True)
-        (-score).backward()
+        (-score).backward() # Loss = Negative log-likelihood
         optimiser.step()
-    print("Epoch %d Score %3.3f KLc %3.3f KLz %3.3f" % (epoch, score.item(), kl.c.item(), kl.z.item()))
+    print("Epoch %d Score %3.3f KLc %3.3f KLz %3.3f" % (epoch+1, score.item(), kl.c.item(), kl.z.item()))
+print("\n" + "-" * 20)
 
-for mu in [-1, 0, 1]:
-    test_D = torch.cat([mu + 0.1*torch.randn(1,1,x_dim) for _ in range(n_inputs)], dim=1)
-    print("\nPosterior predictive for", test_D)
-    print(vhe.sample(inputs={"c":test_D}).x)
+## ----- Testing -----
+def pretty(vector): return "[" + ", ".join("%2.2f" % x for x in vector) + "]"
+for i in range(3):
+    test_class = generateClass()
+    test_D = test_class[:Dc].unsqueeze(0) # Batch size 1
+    print("\nSupport set D = [" + ", ".join(pretty(x) for x in test_D[0]) + "]" )
+    print("Posterior predictive samples:")
+    for j in range(3):
+        x = vhe.sample(inputs={"c":test_D}).x[0]
+        print("  x = " + pretty(x))
--- vhe.py
+++ vhe.py
@@ -8,171 +8,24 @@ import torch
 from torch import nn, distributions
 import numpy as np
 
-def assert_msg(condition, message):
-    if not condition: raise Exception(message)
-
-class CustomDistribution():
-    def make(self, name): raise NotImplementedError()
-
-class NormalPrior(CustomDistribution):
-    class F(nn.Module):
-        def __init__(self, name, size):
-            super().__init__()
-            self.name = name
-            self.size = size
-
-        def forward(self, batch_size=None, **kwargs):
-            assert (batch_size != None) != (self.name in kwargs)
-
-            if self.name in kwargs:  #Don't sample
-                x = kwargs[self.name]
-                if self.size is None:
-                    self.size = list(x.size()[1:])
-                    self.t = x.new_zeros(1,1)
-                dist = distributions.normal.Normal(x.new_zeros(1), x.new_ones(1))
-                batch_size = x.size(0)
-                return Result(kwargs[self.name], dist.log_prob(x).view(batch_size,-1).sum(dim=1))
-            else:
-                size = [batch_size] + self.size
-                dist = distributions.normal.Normal(self.t.new_zeros(size), self.t.new_ones(size))
-                x = dist.rsample()
-                return Result(x, dist.log_prob(x))
-    
-    def __init__(self, sizes=None):
-        self.size = None
-
-    def make(self, name):
-        module = self.F(name, self.size)
-        args = set()
-        return Factor(module, name, args)
-
-class Result:
-    def __init__(self, value, reparam_log_prob=0, reinforce_log_prob=None):
-        self.value = value
-        self.reparam_log_prob = reparam_log_prob
-        self.reinforce_log_prob = reinforce_log_prob
-        self.log_prob = self.reparam_log_prob
-        if self.reinforce_log_prob is not None: self.log_prob += self.reinforce_log_prob
-        
-class DataLoader():
-    VHEBatch = namedtuple("VHEBatch", ["inputs", "sizes", "target"])
-    def __init__(self, data, batch_size, labels, k_shot):
-        self.data = data
-        self.mode = "tensor" if torch.is_tensor(data) else "list"
-        if self.mode == "tensor":
-            self.select_data = lambda x_idx: torch.index_select(self.data, 0, x_idx)
-        else:
-            self.select_data = lambda x_idx: [self.data[i] for i in x_idx]
-        self.labels = {}     # For each label type, a LongTensor assigning elements to labels
-        self.label_idxs = {} # For each label type, for each label, a list of indices
-        for k,v in labels.items():
-            v = list(v)
-            if torch.is_tensor(v[0]): v = [x.item() for x in v]
-            unique_oldlabels = list(set(v))
-            map_label = {oldlabel:label for label, oldlabel in enumerate(unique_oldlabels)}
-            self.labels[k] = torch.LongTensor([map_label[oldlabel] for oldlabel in v])
-            self.label_idxs[k] = {j:[] for j in range(len(unique_oldlabels))} 
-            for i,j in enumerate(self.labels[k]):
-                self.label_idxs[k][j.item()].append(i)
-            for j in range(len(unique_oldlabels)):
-                self.label_idxs[k][j]=torch.LongTensor(self.label_idxs[k][j])
-        self.batch_size = batch_size
-        self.k_shot = k_shot
-
-    def __iter__(self):
-        self.next_i = 0
-        self.x_idx = torch.randperm(len(self.data))
-        return self
-
-    def __next__(self):
-        return self.next()
-
-    def next(self):
-        if self.next_i+self.batch_size > len(self.data):
-            raise StopIteration()
-        else:
-            x_idx = self.x_idx[self.next_i:self.next_i+self.batch_size]
-            self.next_i += self.batch_size
-
-        labels = {k: torch.index_select(self.labels[k], 0, x_idx) for k in self.labels}
-        x = self.select_data(x_idx)
-
-        inputs = {}
-        sizes = {} 
-        for k,v in labels.items():
-            possibilities = [self.label_idxs[k][v[i].item()] for i in range(len(x_idx))]
-            sizes[k] = [len(X) for X in possibilities]
-            input_idx = [np.random.choice(X, size=self.k_shot[k]) for X in possibilities]
-            _inputs = [
-                self.select_data(torch.LongTensor([I[j] for I in input_idx]))
-                for j in range(self.k_shot[k])]
-            if self.mode == "tensor":
-                inputs[k] = torch.cat([x.unsqueeze(1) for x in _inputs], dim=1)
-            elif self.mode == "list":
-                inputs[k] = [[_inputs[j][i] for j in range(self.k_shot[k])]
-                        for i in range(len(_inputs[0]))]
-        return self.VHEBatch(target=x, inputs=inputs, sizes=sizes)
-
-class Factor(namedtuple('Factor', ['module', 'name', 'args'])):
-    def __call__(self, *args, **kwargs):
-        return self.module.forward(*args, **kwargs)
-
-def createFactorFromModule(module):
-    assert 'forward' in dir(module)
-    spec = inspect.getargspec(module.forward)
-    assert spec.defaults == (None,)
-    name = spec.args[-1]
-    args = set([k for k in spec.args[1:-1] if k != "inputs"]) #Not self, name or inputs 
-    return Factor(module, name, args)
-    
-class Factors:
-    def __init__(self, **kwargs):
-        unordered_factors = []
-        for name, f in kwargs.items():
-            if isinstance(f, CustomDistribution):
-                factor = f.make(name)
-            else:
-                factor = createFactorFromModule(f)
-                assert factor.name == name 
-            unordered_factors.append(factor)
-
-        factors = OrderedDict()
-        dependencies = {}
-        try:
-            while len(unordered_factors)>0:
-                factor = next(f for f in unordered_factors if f.args <= set(factors.keys()))
-                factors[factor.name] = factor 
-                dependencies[factor.name] = factor.args | \
-                        set([k for f2 in factor.args for k in dependencies[f2]])
-                unordered_factors.remove(factor)
-        except StopIteration:
-            raise Exception("Can't make a tree out of factors: " + 
-                    "".join("p(" + k + "|" + ",".join(v.args) + ")" for k,v in unordered_factors))
-
-        self.factors = factors
-        self.dependencies = dependencies
-        self.variables = list(self.factors.keys())
-        self.modules = list(f.module for f in self.factors.values())
-
-class Vars():
-    def __init__(self, **kwargs):
-        for k,v in kwargs.items():
-            setattr(self, k, v)
-class KL(Vars):
-    pass
-
 class VHE(nn.Module):
     def __init__(self, encoder, decoder, prior=None):
+        """
+        encoder can be any of: nn.Module, list[nn.Module], dict(class_name -> nn.Module)
+        decoder is an nn.Module
+        prior defaults to Normal(0,1), or can be any of: nn.Module, list[nn.Module], dict(class_name -> nn.Module)
+        """
         super(VHE, self).__init__()
-        self.encoder = encoder
+
+        self.encoder = asFactors(encoder)
         self.decoder = createFactorFromModule(decoder)
-        if prior is not None:
-            assert set(prior.variables) == set(encoder.variables)
-            self.prior = prior
+        if prior is None:
+            self.prior = Factors(**{k:NormalPrior() for k in self.encoder.variables})
         else:
-            self.prior = Factors(**{k:NormalPrior() for k in encoder.variables})
+            self.prior = asFactors(prior)
+            assert set(self.prior.variables) == set(self.encoder.variables)
+            
         self.modules = nn.ModuleList(self.prior.modules + self.encoder.modules + [self.decoder.module])
-    
         self.observation = self.decoder.name 
         self.latents = self.prior.variables 
 
@@ -244,3 +97,221 @@ class VHE(nn.Module):
             raise Exception("Can't find a valid sampling path")
 
         return Vars(**sampled_vars)
+
+
+def assert_msg(condition, message):
+    if not condition: raise Exception(message)
+
+class CustomDistribution():
+    def make(self, name): raise NotImplementedError()
+
+class NormalPrior(CustomDistribution):
+    class F(nn.Module):
+        def __init__(self, name, size):
+            super().__init__()
+            self.name = name
+            self.size = size
+
+        def forward(self, batch_size=None, **kwargs):
+            assert (batch_size != None) != (self.name in kwargs)
+
+            if self.name in kwargs:  #Don't sample
+                x = kwargs[self.name]
+                if self.size is None:
+                    self.size = list(x.size()[1:])
+                    self.t = x.new_zeros(1,1)
+                dist = distributions.normal.Normal(x.new_zeros(1), x.new_ones(1))
+                batch_size = x.size(0)
+                return Result(kwargs[self.name], dist.log_prob(x).view(batch_size,-1).sum(dim=1))
+            else:
+                size = [batch_size] + self.size
+                dist = distributions.normal.Normal(self.t.new_zeros(size), self.t.new_ones(size))
+                x = dist.rsample()
+                return Result(x, dist.log_prob(x))
+    
+    def __init__(self, sizes=None):
+        self.size = None
+
+    def make(self, name):
+        module = self.F(name, self.size)
+        args = set()
+        return Factor(module, name, args)
+
+class Result:
+    def __init__(self, value, reparam_log_prob=0, reinforce_log_prob=None):
+        self.value = value
+        self.reparam_log_prob = reparam_log_prob
+        self.reinforce_log_prob = reinforce_log_prob
+        self.log_prob = self.reparam_log_prob
+        if self.reinforce_log_prob is not None: self.log_prob += self.reinforce_log_prob
+
+
+VHEBatch = namedtuple("VHEBatch", ["inputs", "sizes", "target"])
+class DataLoader():
+    def __init__(self, data, batch_size, labels, k_shot, transforms=[]):
+        self.data = data
+        self.mode = "tensor" if torch.is_tensor(data) else "list"
+        if self.mode == "tensor":
+            self.select_data = lambda x_idx: torch.index_select(self.data, 0, x_idx)
+        else:
+            self.select_data = lambda x_idx: [self.data[i] for i in x_idx]
+        self.labels = {}     # For each label type, a LongTensor assigning elements to labels
+        self.label_idxs = {} # For each label type, for each label, a list of indices
+        for k,v in labels.items():
+            v = list(v)
+            if torch.is_tensor(v[0]): v = [x.item() for x in v]
+            unique_oldlabels = list(set(v))
+            map_label = {oldlabel:label for label, oldlabel in enumerate(unique_oldlabels)}
+            self.labels[k] = torch.LongTensor([map_label[oldlabel] for oldlabel in v])
+            self.label_idxs[k] = {j:[] for j in range(len(unique_oldlabels))} 
+            for i,j in enumerate(self.labels[k]):
+                self.label_idxs[k][j.item()].append(i)
+            for j in range(len(unique_oldlabels)):
+                self.label_idxs[k][j]=torch.LongTensor(self.label_idxs[k][j])
+        self.batch_size = batch_size
+        self.k_shot = k_shot
+        self.transforms = transforms
+
+    def __iter__(self):
+        self.next_i = 0
+        self.x_idx = torch.randperm(len(self.data))
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.next_i+self.batch_size > len(self.data):
+            raise StopIteration()
+        else:
+            x_idx = self.x_idx[self.next_i:self.next_i+self.batch_size]
+            self.next_i += self.batch_size
+
+        labels = {k: torch.index_select(self.labels[k], 0, x_idx) for k in self.labels}
+        x = self.select_data(x_idx)
+
+        inputs = {}
+        sizes = {} 
+        for k,v in labels.items():
+            possibilities = [self.label_idxs[k][v[i].item()] for i in range(len(x_idx))]
+            sizes[k] = [len(X) for X in possibilities]
+            input_idx = [np.random.choice(X, size=self.k_shot[k]) for X in possibilities]
+            _inputs = [
+                self.select_data(torch.LongTensor([I[j] for I in input_idx]))
+                for j in range(self.k_shot[k])]
+            if self.mode == "tensor":
+                inputs[k] = torch.cat([x.unsqueeze(1) for x in _inputs], dim=1)
+            elif self.mode == "list":
+                inputs[k] = [[_inputs[j][i] for j in range(self.k_shot[k])]
+                        for i in range(len(_inputs[0]))]
+
+        batch = VHEBatch(target=x, inputs=inputs, sizes=sizes)
+        for transform in self.transforms:
+            batch = transform.apply(batch)
+        return batch
+
+class Transform():
+    """
+    A set of transformations to apply to a dataset
+    Params:
+        f is the transform function: f(data, args)
+        args is a tensor containing possible transform arguments (so n_transforms = args.size(0))
+        share_labels is the list of latent labels that should be shared between x and the transformed version of x
+    """
+    def __init__(self, f, args, share_labels=None):
+        self.f = f
+        self.args = args
+        self.n_transforms = args.size(0)
+        self.share_labels = [] if share_labels is None else share_labels
+
+    def transform_tensor(self, t):
+        # First dim of t is batch
+        # Applies a random transform to every row of t
+        transform_idxs = t.new_tensor(t.size(0)).random_(0, self.n_transforms).long()
+        transform_args = t.index_select(0, transform_idxs)
+        return self.f(t, transform_args)
+
+    def apply(self, batch):
+        target = self.transform_tensor(batch.target)
+        inputs = {}
+        for k, D in batch.inputs.items():
+            if k in self.share_labels:
+                D_unrolled = D.reshape(D.size(0) * D.size(1), *D.size()[2:])
+                D_transformed = self.transform_tensor(D_unrolled)
+                inputs[k] = D_transformed.reshape(D.size(0), D.size(1), *D_transformed.size()[1:])
+            else:
+                inputs[k] = D
+        sizes = {k: [s*self.n_transforms for s in v] if k in self.share_labels else v
+                for k,v in batch.sizes.items()}
+        return VHEBatch(target=target, inputs=inputs, sizes=sizes)
+
+
+
+
+class Factor(namedtuple('Factor', ['module', 'name', 'args'])):
+    def __call__(self, *args, **kwargs):
+        r = self.module.forward(*args, **kwargs)
+        if Result.__instancecheck__(r):
+            return r
+        else:
+            return Result(*r)
+
+
+def createFactorFromModule(module):
+    assert 'forward' in dir(module)
+    spec = inspect.getargspec(module.forward)
+    assert spec.defaults == (None,)
+    name = spec.args[-1]
+    args = set([k for k in spec.args[1:-1] if k != "inputs"]) #Not self, name or inputs 
+    return Factor(module, name, args)
+    
+class Factors:
+    def __init__(self, *args, **kwargs):
+        unordered_factors = []
+        for f in args:
+            factor = createFactorFromModule(f)
+            unordered_factors.append(factor)
+
+        for name, f in kwargs.items():
+            if isinstance(f, CustomDistribution):
+                factor = f.make(name)
+            else:
+                factor = createFactorFromModule(f)
+                assert factor.name == name 
+            unordered_factors.append(factor)
+
+        factors = OrderedDict()
+        dependencies = {}
+        try:
+            while len(unordered_factors)>0:
+                factor = next(f for f in unordered_factors if f.args <= set(factors.keys()))
+                factors[factor.name] = factor 
+                dependencies[factor.name] = factor.args | \
+                        set([k for f2 in factor.args for k in dependencies[f2]])
+                unordered_factors.remove(factor)
+        except StopIteration:
+            raise Exception("Can't make a tree out of factors: " + 
+                    "".join("p(" + k + "|" + ",".join(v.args) + ")" for k,v in unordered_factors))
+
+        self.factors = factors
+        self.dependencies = dependencies
+        self.variables = list(self.factors.keys())
+        self.modules = list(f.module for f in self.factors.values())
+
+def asFactors(f):
+    if Factors.__instancecheck__(f):
+        return f
+    elif dict.__instancecheck__(f):
+        return Factors(**f)
+    elif "__iter__" in dir(f):
+        return Factors(*f)
+    else:
+        return Factors(f)
+
+class Vars():
+    def __init__(self, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+
+class KL(Vars):
+    pass
